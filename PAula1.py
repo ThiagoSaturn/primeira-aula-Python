6ee6b270717a4e3bdb091c8f22688316a425d7ee
ThiagoSaturn <thiagosattrader@gmail.com>
2024-11-08 11:35:31 -0300

    Update PAula1.py

--- PAula1.py
+++ PAula1.py
@@ -1,1 +1,13 @@
 print("Olá mundo") // comando para imprimir texto na tela
+
+// tipos de entrada
+
+int(inteiro, EX: 1,10,555)
+float(numeros flutuantes, ex: 1,60  50.000,00)
+bolean(verdaeiro ou falso)
+complex(letras e numeros, Ex:f4 * 5 * 9x)
+string(nomes e palavras)
+list(armazena uma lista , Ex:alface, tomate, sacola)
+
+print(type(variavel)) (para descobrir o tipo da entrada de dados)
+//funções 
